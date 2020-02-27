22ab9e262b3d07d565ad3a3f4b39f2a6e7a4f52d
github NikolaCzv <github nikola.raicic@hotmail.com>
2020-02-26 21:11:10 -0600

    user controller, login works

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,17 @@ Rails.application.routes.draw do
   resources :games
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :follows
+      resources :users, only: [:create, :index, :show, :edit, :update, :destroy]
+      resources :reviews
+      resources :games
+      post '/login', to: 'users#login'
+      get '/current_user', to: 'users#show'
+      post '/signup', to: 'users#create'
+      get '/show_user/:id', to: 'users#show_user'
+    end
+  end
 end

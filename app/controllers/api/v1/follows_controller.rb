6cd0654f0a27a8265014fb5cd828a94a21923324
github NikolaCzv <nikola.raicic@hotmail.com>
2020-03-14 15:42:48 -0500

    follow controller ready

--- app/controllers/api/v1/follows_controller.rb
+++ app/controllers/api/v1/follows_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::FollowsController < ApplicationController
+    def index
+        follows = Follow.all
+
+        render json: follows
+    end
+
+    def show
+        follow = Follow.find(params[:id])
+
+        render json: follow
+    end
+
+    def create
+        follow = Follow.create(follower_id: params[:follower_id], followee_id: params[:followee_id])
+
+        render json: follow
+    end
+
+    def update
+        follow = Follow.find(params[:id])
+        follow.update(params.require(:follow).permit(:follower_id, :followee_id))
+
+        render json: follow
+    end
+
+    def destroy_follow
+        follow = Follow.find_by(followee_id: params[:followee_id], follower_id: params[:follower_id]).destroy
+    
+        render json: follow
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :follows
+      resources :follows, only: [:create, :index, :show, :update]
       resources :users, only: [:create, :index, :show, :edit, :update, :destroy]
       resources :reviews
       resources :games
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
       get '/current_user', to: 'users#show'
       post '/signup', to: 'users#create'
       get '/show_user/:id', to: 'users#show_user'
+      delete '/follows/:followee_id/:follower_id', to: 'follows#destroy_follow'
     end
   end
 end
